5392ff2d24e347f7205fa0c88fc9988dfac6f479
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-28 00:28:44 +0530

    Add parser

--- json_parser/parser.py
+++ json_parser/parser.py
@@ -1,13 +1,140 @@
 """Parser functions"""
-from typing import Dict
+from typing import Dict, List, Union
 
-from .lexer import tokenize
+from json_parser.lexer import tokenize
+
+JSONArray = List[object]
+JSONObject = Dict[str, object]
+JSONNumber = Union[int, float]
 
 
-def parse(json_string: str) -> Dict[str, object]:
-    """Parses a json string into a dictionary"""
+class ParseError(Exception):
+    """Error thrown when an invalid JSON tokens is parsed"""
+
+
+def parse_object(tokens: List[str]) -> JSONObject:
+    """Parses an object out of json tokens"""
+    obj: JSONObject = {}
+
+    # special case:
+    if tokens[0] == '}':
+        tokens.pop(0)
+        return obj
+
+    while tokens:
+        token = tokens.pop(0)
+
+        # least amount of tokens left shouldif be a colon, a token and a }
+        if len(tokens) < 3:
+            raise ParseError("Unexpected end of file while parsing")
+
+        if not token.startswith('"'):
+            raise ParseError(
+                "Expected string key for object, found {}".format(token))
+
+        key = parse_string(token)
+
+        token = tokens.pop(0)
+        if token != ':':
+            raise ParseError("Expected colon, found {}".format(token))
+
+        value = _parse(tokens)
+        obj[key] = value
+
+        if not tokens:
+            raise ParseError("Unexpected end of file while parsing")
+
+        token = tokens.pop(0)
+        if token not in ',}':
+            raise ParseError("Expected ',' or '}}', found {}".format(token))
+
+        if token == '}':
+            break
+
+    return obj
+
+
+def parse_array(tokens: List[str]) -> JSONArray:
+    """Parses an array out of json tokens"""
+    array: JSONArray = []
+
+    # special case:
+    if tokens[0] == ']':
+        tokens.pop(0)
+        return array
+
+    while tokens:
+        # least number of tokens left should be a token and a ]
+        if len(tokens) < 2:
+            raise ParseError("Unexpected end of file while parsing")
+
+        value = _parse(tokens)
+        array.append(value)
+
+        token = tokens.pop(0)
+        if token not in ',]':
+            raise ParseError("Expected ',' or ']', found {}".format(token))
+
+        if token == ']':
+            break
+
+    return array
+
+
+def parse_string(token: str) -> str:
+    """Parses a string out of a json token"""
+    return token[1:-1]
+
+
+def parse_number(token: str) -> JSONNumber:
+    """Parses a number out of a json token"""
+    try:
+        if token.isdigit():
+            number: JSONNumber = int(token)
+        else:
+            number = float(token)
+        return number
+
+    except ValueError as err:
+        raise ParseError("Invalid token: {}".format(token)) from err
+
+
+def _parse(tokens: List[str]) -> object:
+    """Recursive JSON parse implementation"""
+    token = tokens.pop(0)
+
+    if token == '[':
+        return parse_array(tokens)
+
+    if token == '{':
+        return parse_object(tokens)
+
+    if token.startswith('"'):
+        return parse_string(token)
+
+    if token[0].isdigit():
+        return parse_number(token)
+
+    special_tokens = {
+        'true': True,
+        'false': False,
+        'null': None,
+    }
+    if token in special_tokens:
+        return special_tokens[token]
+
+    raise ParseError("Unexpected token: {}".format(token))
+
+
+def parse(json_string: str) -> object:
+    """Parses a json string into a Python object"""
     tokens = tokenize(json_string)
 
-    print(tokens)  # TODO: parse
+    value = _parse(tokens)
+    if len(tokens) != 0:
+        raise ParseError("Invalid JSON at {}".format(tokens[0]))
 
-    return {}
+    return value
+
+
+parse('["foo", "bar"]')
--- tests/json_parser_test.py
+++ tests/json_parser_test.py
@@ -10,6 +10,14 @@ import json_parser
     (
         ('{}', {}),
         ('[]', []),
+        ('{"abc": "def"}', {"abc": "def"}),
+        ('{"value": 42}', {"value": 42}),
+        ('{"value": 12.3}', {"value": 12.3}),
+        ('["foo", "bar"]', ["foo", "bar"]),
+        ('[1, 2, 3]', [1, 2, 3]),
+        ('{"value1": true, "value2": false, "value3": null}',
+         {"value1": True, "value2": False, "value3": None}),
+        ('{"foo": [1, 2, {"bar": 3}]}', {"foo": [1, 2, {"bar": 3}]}),
     )
 )
 def test_parser(json_string: str, expected: Dict[str, object]) -> None:
